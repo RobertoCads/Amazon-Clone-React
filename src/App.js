fa427fbdef02caddeadec4e95d54d587851a6925
Roberto Cadenas <robertocads7@gmail.com>
2022-04-14 11:51:09 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./Components/Header/Header.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Estoy en control</h1>
+    <div className="app">
+      <Header/>
     </div>
   );
 }
