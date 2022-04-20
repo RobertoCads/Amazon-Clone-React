06adcc5d1e5ce64b5db4aeefc981011497229ae2
Roberto Cadenas <robertocads7@gmail.com>
2022-04-20 15:20:12 +0200

    checkout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from "./Components/Header/Header.js";
 import Home from "./Components/Home/Home.js";
+import Checkout from "./Components/Checkout/Checkout.js";
+import { Routes, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Header/>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+      </Routes>
     </div>
   );
 }
