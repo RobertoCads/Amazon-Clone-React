248014799ae2cc0e798cc435b37ce94878c213b8
Roberto Cadenas <robertocads7@gmail.com>
2022-04-20 11:44:44 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./Components/Header/Header.js";
+import Home from "./Components/Home/Home.js";
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <Home />
     </div>
   );
 }
