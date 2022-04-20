4b9d818b6734b7d7f4a19bf913b2ae76da066f96
Roberto Cadenas <robertocads7@gmail.com>
2022-04-20 15:54:59 +0200

    checkout

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Subtotal from '../Subtotal/Subtotal';
 import "./Checkout.css";
 
 function Checkout() {
   return (
     <div className='checkout'>
-        <h1>Checkout</h1>
+        <div className='checkout__left'>
+            <img className='checkout__ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'/>
+
+            <div>
+                <h2 className="checkout__title">
+                    Your Shopping Basket
+                </h2>
+            </div>
+        </div>
+
+        <div className='checkout__right'>
+            <Subtotal />
+        </div>
     </div>
   )
 }
